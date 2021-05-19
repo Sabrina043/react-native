e39df4a5d8ce9e6baca78bde5efd5252ae167317
sarah <slachehab6@gmail.com>
2021-05-19 12:11:48 +0200

    fin exo3

--- Jour_1/test/App.js
+++ Jour_1/test/App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Pressable } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Pressable, Button, showAlert,Alert } from 'react-native';
 
 export default function App() {
+
+
+  
+
+
 
   return (
     <ScrollView style={styles.container}>
@@ -20,13 +25,23 @@ export default function App() {
         <Image source={require("./images/konexio-logo_1.png")}
           style={{ width: 315, height: 100 }} />
 
+    [
+
 
 
       </View>
 
-        <Pressable onPress={styles.onPress}>
-          <Button >I'm pressable!</Button>
-        </Pressable>
+{/* const App = () => (      */}
+        <View style={styles.container}>
+          {/* <Button title="Show alert" onPress={showAlert} Alert= {Alert}/> */}
+          <Button
+          onPress={() => {
+            alert('You tapped the button!');
+          }}
+          title="Press Me" color="#fff9"
+        />
+          
+        </View>
 
     </ScrollView>
   );
@@ -38,27 +53,29 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     // alignItems: 'center',
     // justifyContent: 'center',
-
+    
   },
   textColor: {
-
+    
     backgroundColor: '#fff',
-
+    
   },
   premier: {
-
+    
     fontSize: '30px',
-
+    
   },
   deuxième: {
-
+    
     textAlign: 'center',
-
+    
   },
   troisième: {
-
+    
     fontWeight: 'bold',
-
+    
   },
+  
 });
+
 
